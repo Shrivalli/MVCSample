eca3d7dca3853e86eea400852cb49e4b9cea6d4c
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2020-07-31 20:22:38 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCSample.Models;
+
+namespace MVCSample.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        List<Emp> employees = new List<Emp>();
+        // GET: Employee
+        public ActionResult GetAllEmployees()
+        {
+            employees = Emp.getEmps();
+            return View(employees);
+        }
+    }
+}
---
+++ Models/Emp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCSample.Models
+{
+    public class Emp
+    {
+        public int Eid { get; set; }
+        public string Ename { get; set; }
+        public float Salary { get; set; }
+
+        public Emp(int eid,string ename,float sal)
+        {
+            Eid = eid;
+            Ename = ename;
+            Salary = sal;
+        }
+
+        public static List<Emp> getEmps()
+        {
+            List<Emp> employees = new List<Emp>();
+            employees.Add(new Emp(1, "Ram", 87000));
+            employees.Add(new Emp(2, "Raghav", 54000));
+            employees.Add(new Emp(3, "Suresh", 23000));
+            employees.Add(new Emp(4, "Ramesh", 98000));
+            return employees;
+        }
+    }
+}
